08ce591358c271b05e7a4a056cf69433e0792a5e
guyuanshan1989 <guyuanshan1989@163.com>
2018-10-26 16:03:27 +0800

    TabBar设置完成

--- SwiftWeibo/SwiftWeibo/MainViewController.swift
+++ SwiftWeibo/SwiftWeibo/MainViewController.swift
@@ -9,27 +9,32 @@
 import UIKit
 
 class MainViewController: UITabBarController {
-
+    
+    private lazy var midButton = UIButton()
+    @IBOutlet weak var myTabBar: UITabBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        setupmidButton()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @objc private func addNewItem() {
+        print("添加新微博")
     }
-    */
-
+    
+    //MARK: - 给tabbar中间添加一个按钮
+    private func setupmidButton() {
+        //设置midButton的背景图片
+        let midButtonImage = UIImage(named: "first")
+        midButton.setImage(midButtonImage, for: .normal)
+        //将midButton添加到myTabBar
+        myTabBar.addSubview(midButton)
+        let count = CGFloat(childViewControllers.count)
+        //让minButton位于正中间并且完全覆盖原来的BarButtonItem
+        let width = myTabBar.bounds.width/count - 1
+        midButton.frame = myTabBar.bounds.insetBy(dx: 2*width, dy: 0)
+        //按钮监听方法
+        midButton.addTarget(self, action: #selector(addNewItem), for: .touchUpInside)
+    }
+    
 }
